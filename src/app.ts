39cce58fbd94f15be008441b329f7ccfcd475592
ulilabzr <absoruu@gmail.com>
2026-06-18 02:24:40 +0700

    feat: securing api by add CORS, helmet, and rate limiting

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express, { Request, Response } from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import { rateLimit } from 'express-rate-limit';
 import { apiReference } from '@scalar/express-api-reference';
 import openApiSpec from './docs/openapi.json';
 
@@ -16,6 +19,27 @@ import employeeRoutes from './routes/employeeRoutes';
 const app = express();
 
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.NODE_ENV === 'production' 
+    ? 'https://domain-asli-frontend.com' 
+    : '*', 
+  optionsSuccessStatus: 200
+}));
+
+app.use(helmet());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 menit
+  max: 100, // Batas maksimal 100 permintaan per windowMs per IP
+  message: "Terlalu banyak permintaan dari IP ini, silakan coba lagi nanti."
+});
+
+app.use(limiter);
+
+app.get('/', (req, res) => {
+  res.send('API aman dengan CORS, Helmet, dan Rate Limiting!');
+});
 
 app.use('/docs', apiReference({
   title: 'Dokumentasi API Be Rukun Jaya',
@@ -32,11 +56,7 @@ app.use('/api/v1/reports', reportRoutes);
 app.use('/api/v1/audit-logs', auditRoutes);
 app.use('/api/v1/employees', employeeRoutes);
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
-});
-
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server berjalan di http://localhost:${PORT}`);
-});
+}); 
