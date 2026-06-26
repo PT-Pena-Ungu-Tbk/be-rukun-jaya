bb3e3c13f6a86c5cabe696cafe3a416243a61454
ulilabzr <absoruu@gmail.com>
2026-06-26 13:07:00 +0700

    feat: implement audit and transaction route controllers and define API contract documentation

--- src/routes/transactionRoutes.ts
+++ src/routes/transactionRoutes.ts
@@ -1,11 +1,11 @@
-// src/routes/transactionRoutes.ts
 import express from 'express';
 const router = express.Router();
-import { checkout, returnItem, getTransactionDetails } from '../controllers/transactionController';
+import { checkout, returnItem, getTransactionDetails, exportTransactionsExcel } from '../controllers/transactionController';
 import { verifyToken, hasRoles } from '../middlewares/authMiddleware';
 
 router.post('/', verifyToken, hasRoles(['MANAGER', 'CASHIER']), checkout);
 router.get('/:transaction_id', verifyToken, hasRoles(['MANAGER', 'CASHIER', 'OWNER']), getTransactionDetails);
 router.post('/return', verifyToken, returnItem);
+router.get('/export/excel', verifyToken, hasRoles(['MANAGER', 'OWNER']), exportTransactionsExcel);
 
 export default router;
